57dec043daa4c64873c3f600dcd28ff5ce90e0f3
d.kobayashi <daiki.kobayashi1910@gmail.com>
2024-11-24 22:56:01 +0900

    update: first commit

---
+++ Server/Services/TestService.cs
@@ -0,0 +1,15 @@
+﻿using MagicOnion;
+using MagicOnion.Server;
+using Shared.Services;
+
+#pragma warning disable CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます
+
+namespace Server.Services;
+
+public class TestService : ServiceBase<ITestService>, ITestService
+{
+    public async UnaryResult<int> AddAsync(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ Shared/Services/ITestService.cs
@@ -0,0 +1,8 @@
+﻿using MagicOnion;
+
+namespace Shared.Services;
+
+public interface ITestService : MagicOnion.IService<ITestService>
+{
+    public UnaryResult<int> AddAsync(int a, int b);
+}
